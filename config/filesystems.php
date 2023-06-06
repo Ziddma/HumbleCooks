9aabc02b21b0bf77e99ee3be09de76d7f9f5e2df
Endra <endra.kartika.25@gmail.com>
2023-06-06 18:18:55 +0700

    changing filesystems to upcoming ingredients crud

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,6 +51,13 @@ return [
             'visibility' => 'public',
         ],
 
+        'ingredients' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/ingredients'),
+            'url' => env('APP_URL') . '/storage', // Update the URL to include '/storage/avatars'
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
