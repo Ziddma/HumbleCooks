e6e9e9930cdbf0faa3432a70c438ea94b6cc079a
Endra <endra.kartika.25@gmail.com>
2023-06-25 23:31:22 +0700

    🐛 Fix image not store to database

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -28,12 +28,14 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
+
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'avatar' => ['required', 'image'],
         ]);
 
         $user = User::create([
@@ -42,6 +44,13 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
             'role_id' => 2,
         ]);
+
+        if ($request->hasFile('avatar')) {
+            $avatar = $request->file('avatar');
+            $avatarPath = $avatar->store('avatars', 'public');
+            $user->avatar = $avatarPath;
+            $user->save();
+        }
 
         event(new Registered($user));
 
